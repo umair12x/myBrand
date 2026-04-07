5b1301bc80d4d4e46e99f8444fb67400287f1119
mian95 <umairim24@gmail.com>
2026-04-07 12:11:43 +0500

    monogdb atlas url fix

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,8 +4,6 @@ export const connectDB = async () => {
     try {
         
         await mongoose.connect(process.env.MONGO_URI, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
           serverSelectionTimeoutMS: 10000,
         });
         console.log('Database connected successfully');
